b07c224f46059ec8cc8c37c2f12b89361d93a435
shu1r0 <shimatani180231n@gmail.com>
2022-06-16 12:34:02 +0900

    add create_vm.sh

---
+++ clos-node4/create_vm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run with sudo" ; exit 1 ; fi
+
+
+create() {
+  virsh create leaf1/sonic.xml
+  virsh create leaf2/sonic.xml
+  virsh create spine1/sonic.xml
+  virsh create spine2/sonic.xml
+}
+
+
+delete() {
+  virsh destroy leaf1
+  virsh destroy leaf2
+  virsh destroy spine1
+  virsh destroy spine2
+}
+
+while getopts "cd" ARGS;
+do
+    case $ARGS in
+    c )
+        create
+        exit 1;;
+    d )
+        delete
+        exit 1;;
+    esac
+done
