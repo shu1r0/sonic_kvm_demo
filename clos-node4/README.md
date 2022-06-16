fbd1c4413cb02d2a925684d4bcd712d16c6bfa94
shu1r0 <shimatani180231n@gmail.com>
2022-06-16 12:28:33 +0900

    first commit

---
+++ clos-node4/README.md
@@ -0,0 +1,9 @@
+# clos
+
+
+## Management Node
+
+install ubuntu.
+```bash
+$ wget "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
+```
---
+++ clos-node4/prepare_bridge.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run with sudo" ; exit 1 ; fi
+
+
+create() {
+  ip link add br_mgmt type bridge
+  ip link set br_mgmt up
+  ip link add br_s1l1 type bridge
+  ip link set br_s1l1 up
+  ip link add br_s1l2 type bridge
+  ip link set br_s1l2 up
+  ip link add br_s2l1 type bridge
+  ip link set br_s2l1 up
+  ip link add br_s2l2 type bridge
+  ip link set br_s2l2 up
+}
+
+
+delete() {
+  ip link delete br_mgmt type bridge
+  ip link delete br_s1l1 type bridge
+  ip link delete br_s1l2 type bridge
+  ip link delete br_s2l1 type bridge
+  ip link delete br_s2l2 type bridge
+}
+
+while getopts "cd" ARGS;
+do
+    case $ARGS in
+    c )
+        create
+        exit 1;;
+    d )
+        delete
+        exit 1;;
+    esac
+done
